c16bbfaa728d6791368cdf6f5d45710ccdacb8c9
naser <nasreddine.elharcha_external@hse.com>
2025-05-16 02:31:37 +0100

    test

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -1,23 +1,9 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
-import { getUserFromRequest } from '../../../../lib/getUserFromRequest'; // Ensure correct import path
+import { withAuthentication } from '@/app/utils/auth.utils' // Ensure correct import path
 
-export async function GET(req: NextRequest) {
-  try {
-    const { userId } = await getUserFromRequest(req);  // Destructure userId correctly
-
-    // Use userId to fetch user details
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-    });
-
-    if (!user) {
-      return NextResponse.json({ error: 'User not found' }, { status: 404 });
-    }
-
-    return NextResponse.json({ user });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+export const GET = withAuthentication(async (req, user) => {
+  return {
+    id: user.id,
+    email: user.email,
+    role: user.role,
   }
-}
+})
